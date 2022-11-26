d96f85b59cd52fa12d0898fbe36b2a3733058872
EwennAnsquer <95921626+EwennAnsquer@users.noreply.github.com>
2022-11-27 00:19:16 +0100

    responsive et ajouter des sliders

--- assets/phpIncludes/navbar.php
+++ assets/phpIncludes/navbar.php
@@ -1,14 +1,16 @@
 <div class="navbar">
     <a href="index.php" id="navbarBrand">BTS SIO 1</a>
-    <div class="categorie">
-        <a href="">Développement</a> 
-        <a href="">Cyber-Sécurité</a>
-        <a href="">Jeux-Vidéos</a>
-        <a href="">Réseau</a>
-    </div>
-    <div class="admin">
-        <a href="">Administrateur</a>
-        <a href="">Contributeur</a>
+    <div class="hamburgerMenu">
+        <div class="categorieNav">
+            <a class="navBarLink">Développement</a> 
+            <a class="navBarLink">Cyber-Sécurité</a>
+            <a class="navBarLink">Jeux-Vidéos</a>
+            <a class="navBarLink">Réseau</a>
+        </div>
+        <div class="admin">
+            <a class="navBarLink" href="">Administrateur</a>
+            <a class="navBarLink" href="">Contributeur</a>
+        </div>
     </div>
 </div>
 
--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -1,10 +1,31 @@
 const navbar = document.querySelector(".navbar")
+const navLink = [...document.querySelectorAll(".categorieNav a")]
+const categorieTitle = [...document.querySelectorAll(".categorie h2")]
 
 function moveNavBar(num) {
     navbar.style.transform = "translateY(" + num + "px)";
 }
 
+function getScrollValue(){
+    scroll = document.documentElement.scrollTop
+    totalheight = document.documentElement.scrollHeight; //hauteur total du document
+    clientHeight = document.documentElement.clientHeight;// hauteur total de la 'fenetre' du client
+    scrollPercentage = Math.round(scroll * 100 / (totalheight - clientHeight))
+    return scrollPercentage
+}
+
+function navbarStyle(){
+    let scrollValue=getScrollValue();
+    if(scrollValue>5){
+        navbar.style.boxShadow="0px 15px 20px 0px rgb(0 0 0 / 10%)";
+    }else{
+        navbar.style.boxShadow="none";
+    }
+}
+
 var lastScrollTop = 0;
+
+navbarStyle()
 
 window.addEventListener("wheel",()=>{
     var st = window.pageYOffset || document.documentElement.scrollTop;
@@ -14,4 +35,13 @@ window.addEventListener("wheel",()=>{
         moveNavBar(0)
     }
     lastScrollTop = st;
+    navbarStyle()
 });
+
+navLink.forEach(e => {
+    e.addEventListener("click",()=>{
+        window.scrollTo(0,categorieTitle[navLink.indexOf(e)].getBoundingClientRect().top + window.scrollY - 120) 
+    })
+});
+
+//va scroller verticalement avec le resultat du nombre de pixel de distance avec le haut de la vue du client + le nombre de pixel scroll sur la page - 120 pour ne pas avoir la navbar qui cache tout
